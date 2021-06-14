21910671961ae648a720c04d9090101ba8aea4e1
Georgii <49598870+corradowaver@users.noreply.github.com>
2021-06-14 12:47:11 +0300

    local

--- src/main/java/lab1/LabOne.java
+++ src/main/java/lab1/LabOne.java
@@ -9,7 +9,7 @@ import static java.util.stream.Collectors.toList;
 
 public class LabOne {
 
-  private static final int N = 1000;
+  private static final int N = 10000;
 
   public static void main(String[] args) throws IOException {
     List<Double> pseudoRandomList = getPseudoRandomList(N);
